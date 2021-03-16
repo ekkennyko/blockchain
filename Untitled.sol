08d73c0127b599cea05ba1b4dfd1a126ef9bd907
Aleksandr Dvorakovskiy <aleksandr.dvorakovskiy@mytona.com>
2021-03-16 13:07:11 +0900

    fix

--- Untitled.sol
+++ Untitled.sol
@@ -1,6 +1,33 @@
 pragma solidity ^0.7.4;
 
-contract HomeList
+contract Owned
+{
+    constructor()
+    {
+        owner = msg.sender;
+    }
+    address private owner;
+    modifier OnlyOwner
+    {
+        require(
+            msg.sender == owner,
+            'Only owner can do it'
+            );
+            _;
+    }
+    
+    function ChangeOwner(address newOwner) public OnlyOwner
+    {
+        owner = newOwner;
+    }
+    
+    function GetOwner() public returns (address)
+    {
+        return owner;
+    }
+}
+
+contract HomeList is Owned
 {
     enum RequestType { NewHome, EditHome }
     
@@ -39,23 +66,30 @@ contract HomeList
     
     struct Employee
     {
-        uint id;
         string name;
         string position;
         string phoneNumber;
         bool isEmployee;
     }
     
-    mapping(uint => Employee) private employees;
+    mapping(address => Employee) private employees;
     mapping(address => Owner) private owners;
     mapping(address => Request) private requests;
     
     mapping(string => Home) private homes;
     mapping(string => Ownership[]) private ownerships;
     
-    uint countIDEmployees = 0;
+    modifier OnlyEmployee
+    {
+        require(
+            employees[msg.sender].isEmployee != false,
+            'Only Employee can do it'
+            );
+            _;
+    }
     
-    function AddHome(string memory _adr, uint _area, uint _cost) public {
+    function AddHome(string memory _adr, uint _area, uint _cost) public
+    {
         Home memory h;
         h.homeAddress = _adr;
         h.area = _area;
@@ -63,39 +97,42 @@ contract HomeList
         homes[_adr] = h;
     }
     
-    function GetHome(string memory adr) public returns (uint _area, uint _cost){
+    function GetHome(string memory adr) public returns (uint _area, uint _cost)
+    {
         return (homes[adr].area, homes[adr].cost);
     }
     
-    function isEmployee(uint _id) private returns(bool _isEmployee){
-        return employees[_id].isEmployee;
+    function isEmployee(address empl) private returns(bool _isEmployee)
+    {
+        return employees[empl].isEmployee;
     }
     
-    function AddEmployee(string memory _name, string memory _position, string memory _phoneNumber) public {
+    function AddEmployee(address empl, string memory _name, string memory _position, string memory _phoneNumber) public OnlyOwner
+    {
         Employee memory e;
-        e.id = countIDEmployees;
         e.name = _name;
         e.position = _position;
         e.phoneNumber = _phoneNumber;
         e.isEmployee = true;
-        employees[countIDEmployees] = e;
-        countIDEmployees++;
+        employees[empl] = e;
     }
     
-    function GetEmployee(uint id) public returns (string memory _name, string memory _position, string memory _phoneNumber){
-        return (employees[id].name, employees[id].position, employees[id].phoneNumber);
-    }
-    
-    function UpdateEmployee(uint _searchId, string memory _newName, string memory _newPosition, string memory _newPhoneNumber) public {
-        string memory empty = "";
-        if(!isEmployee(_searchId)) revert();
-        if(bytes(_newName).length != bytes(empty).length) {  employees[_searchId].name = _newName; }
-        if(bytes(_newPosition).length != bytes(empty).length) { employees[_searchId].position = _newPosition; }
-        if(bytes(_newPhoneNumber).length != bytes(empty).length) { employees[_searchId].phoneNumber = _newPhoneNumber; }
-    }
-    
-    function RemoveEmployee(uint id) public
+    function GetEmployee(address empl) public OnlyOwner returns (string memory _name, string memory _position, string memory _phoneNumber)
     {
-        delete employees[id];
+        return (employees[empl].name, employees[empl].position, employees[empl].phoneNumber);
+    }
+    
+    function UpdateEmployee(address empl, string memory _newName, string memory _newPosition, string memory _newPhoneNumber) public OnlyOwner
+    {
+        string memory empty = "";
+        if(!isEmployee(empl)) revert();
+        if(bytes(_newName).length != bytes(empty).length) {  employees[empl].name = _newName; }
+        if(bytes(_newPosition).length != bytes(empty).length) { employees[empl].position = _newPosition; }
+        if(bytes(_newPhoneNumber).length != bytes(empty).length) { employees[empl].phoneNumber = _newPhoneNumber; }
+    }
+    
+    function RemoveEmployee(address empl) public OnlyOwner
+    {
+        delete employees[empl];
     }
 }
